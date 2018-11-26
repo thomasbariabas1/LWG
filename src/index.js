af18db8ae0ce3382c39d5cd4fa154645677a0ccd
thomasbariabas1 <thomas.bariabas@gmail.com>
2018-11-26 23:25:13 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { history } from "./lib/history";
+import { Router, Route, Switch } from "react-router-dom";
+
+import indexRoutes from "routes/index.jsx";
+
+import "assets/scss/material-kit-react.css?v=1.3.0";
+
+
+ReactDOM.render(
+  <Router history={history}>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} key={key} component={prop.component} />;
+      })}
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
---
+++ src/lib/history/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by thoriampas on 11/26/2018.
+ */
+import { createBrowserHistory } from "history";
+
+export const history = createBrowserHistory();
+
+// Get the current location.
+const location = history.location;
+
+// Listen for changes to the current location.
+const unlisten = history.listen((location, action) => {
+    // location is an object like window.location
+    console.log(action, location.pathname, location.state);
+});
+
+// Use push, replace, and go to navigate around.
+export const push =(location) => history.push(location)
+
+// To stop listening, call the function returned from listen().
+unlisten();
